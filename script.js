73051e3dd7f5f537c1158391fe4a3789e05ffe51
Sandra <hi-sandra@hotmail.com>
2026-06-12 21:26:28 -0600

    busqueda de iniciales

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const invitados = ["Ailyn", "Berenice", "Carolina", "Cesar", "Dessire", "Gabriel
 function encontrarPareja(arr) {
     let inicio = 0;
     let siguiente = 1;
+
+    while (siguiente < arr.length) {
+        if (arr[inicio][0] === arr[siguiente][0]) {
+            return [arr[inicio], arr[siguiente]];
+        }
+        inicio++;
+        siguiente++;
+    }
