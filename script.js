b3a9ef3756733ff52cc470727e8314d0c8b9b131
Sandra <hi-sandra@hotmail.com>
2026-06-12 21:23:37 -0600

    funcion

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 const invitados = ["Ailyn", "Berenice", "Carolina", "Cesar", "Dessire", "Gabriel", "Gaby", "Isaac", "Marcos", "Miguel", "Yakelin"]
+
+function encontrarPareja(arr) {
+    let inicio = 0;
+    let siguiente = 1;
